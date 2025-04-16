5085d28ba71ff1f24c67fc3e2b4533f1ddbad100
Akash <142076240+egrvksh1008@users.noreply.github.com>
2025-04-16 11:33:43 +0530

    Update main.tf
    updated VPC

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,26 @@
+# Define the AWS provider configuration.
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
 
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "Pub-sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.1/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "Private-sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.2/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = False
+}
